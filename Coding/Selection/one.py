9416bd401b2cf1d97abbfb90ed36abfc97f131ff
sigmapi-academy <kumarsatyendermishra@gmail.com>
2026-05-06 12:20:10 +0530

    Discussed Selection in Python, Homework questions included

---
+++ Coding/Selection/one.py
@@ -0,0 +1,5 @@
+cost = float(input('Enter the price of pen: \u20b9'))
+if cost <= 100:
+    print('Give me the pen.')
+else:
+    print('Show me another pen, whose price is less than or equal to \u20b9100')
---
+++ Coding/Selection/two.py
@@ -0,0 +1,8 @@
+num = int(input('Enter number: '))
+
+if num == 0:
+    print('Zero')
+elif num > 0:
+    print('positive')
+else:
+    print('negative')
